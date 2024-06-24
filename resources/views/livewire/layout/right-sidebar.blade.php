87c74555e77a695d9d882c356f11bd67567a73c9
badru2 <hilalbadru4@gmail.com>
2024-06-24 15:13:56 +0700

    FRONTEND: left side bar

---
+++ resources/views/livewire/layout/right-sidebar.blade.php
@@ -0,0 +1,6 @@
+<div id="right-sidebar" class="absolute right-0 w-[32%] border-l border-gray-800 min-h-screen">
+    <div class="w-[86%] mx-auto flex flex-col items-center mt-2">
+        <input type="text" placeholder="Search" class="w-full px-5 py-2 rounded-full border-slate-800 bg-slate-800">
+
+    </div>
+</div>
